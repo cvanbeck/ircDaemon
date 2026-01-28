07b68aabcca2041df6029402f05a75b6dedc3adc
Charlie <charlievanbeck@gmail.com>
2026-01-28 20:57:59 +0000

    Implemented basic echo server based on Java docs

--- src/main/java/com/github/cvanb002/Server.java
+++ src/main/java/com/github/cvanb002/Server.java
@@ -1,7 +1,34 @@
 package com.github.cvanb002;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        int port = 6665; // ports 6665-6669 are often reserved for IRC
+
+        try(
+            ServerSocket serverSocket = new ServerSocket(port, 0, InetAddress.getByName(null));
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream())
+            )
+        ){
+            String inputLine;
+            while((inputLine = in.readLine()) != null){
+                out.println(inputLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+
     }
 }
---
+++ src/main/java/com/github/cvanb002/testClient.java
@@ -0,0 +1,33 @@
+package com.github.cvanb002;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class testClient {
+    public static void main(String[] args){
+        int port = 6665;
+        String host = "127.0.0.1";
+        try(
+            Socket socket = new Socket(host, port);
+            PrintWriter out =
+                new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+            BufferedReader stdIn =
+                new BufferedReader(
+                        new InputStreamReader(System.in));
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null){
+                out.println(userInput);
+                System.out.println("Echo: " + in.readLine());
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
