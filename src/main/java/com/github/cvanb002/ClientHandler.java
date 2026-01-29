4fe34b06821a280b2de15bf456e14efdb310f947
Charlie <charlievanbeck@gmail.com>
2026-01-29 19:09:54 +0000

    Reduce thread usage for each connection in ClientHandler

--- src/main/java/com/github/cvanb002/ClientHandler.java
+++ src/main/java/com/github/cvanb002/ClientHandler.java
@@ -2,75 +2,86 @@ package com.github.cvanb002;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 
-public class ClientHandler extends Thread{
+public class ClientHandler {
     Socket clientSocket = null;
+    MessageCentre messages;
 
-    ClientHandler(Socket client){
-        super("ServerThread");
+    ClientHandler(Socket client, MessageCentre messages) {
         clientSocket = client;
+        this.messages = messages;
     }
 
-    public void run(){
-        try(
-            InputThread in = new InputThread(clientSocket.getInputStream());
-            OutputThread out = new OutputThread(clientSocket.getOutputStream());
+    public void run() {
+        try (
+                InputThread in = new InputThread(clientSocket.getInputStream(), messages);
+                OutputThread out = new OutputThread(clientSocket.getOutputStream(), messages);
         ) {
             in.start();
             out.start();
-            while(in.getState() != Thread.State.TERMINATED){
+            while (in.getState() != Thread.State.TERMINATED) {
                 continue;
             }
             clientSocket.close();
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace(System.out);
         }
     }
-}
 
-class InputThread extends Thread implements AutoCloseable {
-    BufferedReader in = null;
 
-    InputThread(InputStream in){
-        super("InputThread");
-        this.in = new BufferedReader(new InputStreamReader(in));
-    }
+    private class InputThread extends Thread implements AutoCloseable {
+        BufferedReader in = null;
+        MessageCentre messageCentre;
 
-    public void run(){
-        String inputLine;
-        try {
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println(inputLine); //tmp
+        InputThread(InputStream in, MessageCentre messageCentre) {
+            super("InputThread");
+            this.in = new BufferedReader(new InputStreamReader(in));
+            this.messageCentre = messageCentre;
+        }
+
+        public void run() {
+            String inputLine;
+            try {
+                while ((inputLine = in.readLine()) != null) {
+                    messageCentre.addMessage(inputLine);
+                }
+            } catch (IOException e) {
+                e.printStackTrace(System.out);
             }
-        } catch (IOException e){
-            e.printStackTrace(System.out);
+        }
+
+        @Override
+        public void close() throws IOException {
+            in.close();
         }
     }
 
-    @Override
-    public void close() throws IOException {
-        in.close();
+    private class OutputThread extends Thread implements AutoCloseable {
+        PrintWriter out;
+        MessageCentre messageCentre;
+
+        OutputThread(OutputStream out, MessageCentre messageCentre) {
+            super("OutputThread");
+            this.out = new PrintWriter(out, true);
+            this.messageCentre = messageCentre;
+        }
+
+        public void run() {
+            while (connected) {
+                out.println(message);
+            }
+        }
+
+        private void difference(ArrayList<String> oldMessages, ArrayList<String> newMessages) {
+            return;
+        }
+
+        @Override
+        public void close() throws IOException {
+            out.close();
+        }
     }
 }
-
-class OutputThread extends Thread implements AutoCloseable{
-    PrintWriter out;
-
-    OutputThread(OutputStream out){
-        super("OutputThread");
-        this.out = new PrintWriter(out, true);
-    }
-
-    public void run(){
-
-    }
-
-    @Override
-    public void close() throws IOException {
-        out.close();
-    }
-}
-
-
 
