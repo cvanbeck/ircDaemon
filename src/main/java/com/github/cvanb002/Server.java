0d81aa22b50f4dc4b600c03946e639f9f2635542
Charlie <charlievanbeck@gmail.com>
2026-01-29 19:10:30 +0000

    Add copyOnWrite array to keep track of multiple connections

--- src/main/java/com/github/cvanb002/Server.java
+++ src/main/java/com/github/cvanb002/Server.java
@@ -2,15 +2,21 @@ package com.github.cvanb002;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 public class Server {
     public static void main(String[] args) throws IOException {
+        MessageCentre messages = new MessageCentre();
+        CopyOnWriteArrayList<ClientHandler> clients = new CopyOnWriteArrayList<>();
+
         int port = 6665; // ports 6665-6669 are often reserved for IRC
 
         try(ServerSocket serverSocket = new ServerSocket(port, 0, InetAddress.getByName(null));){
             while(true){
-                new ServerThread(serverSocket.accept()).start();
+                ClientHandler client = new ClientHandler(serverSocket.accept(), messages);
+                client.start();
+                clients.add(client);
             }
         } catch (IOException e) {
             e.printStackTrace(System.out);
