4624aec39b6fdd105bace11da25414201d4cf18f
Charlie <charlievanbeck@gmail.com>
2026-01-29 16:27:32 +0000

    Refactor ServerThread to use a seperate thread for input

--- src/test/java/com/github/cvanb002/testClient.java
+++ src/test/java/com/github/cvanb002/testClient.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 
 public class testClient {
     public static void main(String[] args){
         int port = 6665;
+        String name = String.valueOf(new Random().nextInt(9999));
         String host = "127.0.0.1";
         try(
             Socket socket = new Socket(host, port);
@@ -22,12 +24,11 @@ public class testClient {
         ) {
             String userInput;
             while ((userInput = stdIn.readLine()) != null){
-                out.println(userInput);
-                System.out.println("Echo: " + in.readLine());
+                out.println(name + ": " +userInput);
+//                System.out.println(in.readLine());
             }
         } catch (IOException e){
             return;
         }
-
     }
 }
