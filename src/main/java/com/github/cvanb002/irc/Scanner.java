060aff02260e430cce172878a9ed782288684de0
Charlie <charlievanbeck@gmail.com>
2026-02-26 21:27:59 +0000

    Fix bugs to ensure passed test
    Added a check for when the scanner is at the end of the message, otherwise messages that don't end with an extended parameter will crash.
    Removed stripping of CLRF as it's not needed.
    Fixed issue where parameters wont work if there is only a command, as it was set to check for more than 1 previous token, as opposed to 1 or more.
    Also fixed isCommand not actually checking if a token is a source or tag first, which was causing problems when a message didn't have either of these.
    Moved early return from contains outside of forEach loop

--- src/main/java/com/github/cvanb002/irc/Scanner.java
+++ src/main/java/com/github/cvanb002/irc/Scanner.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 
-public class Scanner{
+public class Scanner {
     // TODO: THis implementation is a mess, defo needs a rework
     String input = "";
     List<Token> tokens = new ArrayList<>();
@@ -12,18 +12,18 @@ public class Scanner{
     private int start = 0;
     private int current = 0;
 
-    public Scanner(String input){
+    public Scanner(String input) {
         this.input = input;
     }
 
 
-    public IRC.Numerics parse(){
+    public IRC.Numerics parse() {
         /* TODO: Need to rewrite this, I want to use enums for numerics but unsure if this is where I should add them */
-        if(inputTooLong()){
+        if (inputTooLong()) {
             return IRC.Numerics.ERR_INPUT_TOO_LONG;
         }
 
-        if(!containsCRLF()){
+        if (!containsCRLF()) {
             return IRC.Numerics.ERR_UNKNOWNERROR;
         }
 
@@ -34,91 +34,90 @@ public class Scanner{
     public List<Token> scanTokens() {
         // TODO: Ugly function, redo later
         String token;
-        while(!finished()){
+        while (!finished()) {
             char c = input.charAt(current);
 
-            if(c == IRC.Constants.EXTENDEDPARAMPREPEND && !isSource(input.substring(current))){
-                token = input.substring(start).strip();
+            if (c == IRC.Constants.EXTENDEDPARAMPREPEND && !isSource(input.substring(current))) {
+                token = input.substring(start, input.length() - 4).strip();
                 addToken(token, IRC.Type.PARAMETER);
                 return tokens;
             }
 
-            if(c == IRC.Constants.SEPERATOR){
+            if (c == IRC.Constants.SEPERATOR || endOfLine()) {
                 token = input.substring(start, current).strip();
 
-                if (isTag(token)){
+                if (isTag(token)) {
                     addToken(token, IRC.Type.TAG);
-                }
-                else if (isSource(token)){
+                } else if (isSource(token)) {
                     addToken(token, IRC.Type.SOURCE);
-                }
-                else if (isParameter()){
+                } else if (isParameter()) {
                     addToken(token, IRC.Type.PARAMETER);
-                }
-                else if (isCommand()){
+                } else if (isCommand(token)) {
                     addToken(token, IRC.Type.COMMAND);
-                }
-                else {
-                    addToken (token, IRC.Type.NULL);
+                } else {
+                    addToken(token, IRC.Type.NULL);
                 }
                 start = current;
             }
             current++;
         }
 
+
         return tokens;
     }
 
-    private void addToken(String token, IRC.Type type){
+    private void addToken(String token, IRC.Type type) {
         tokens.add(new Token(token, type));
     }
 
-    private boolean isTag(String input){
+    private boolean isTag(String input) {
         return (tokens.isEmpty() && input.charAt(0) == IRC.Constants.TAGSPREPEND);
     }
 
-    private boolean isSource(String input){
+    private boolean isSource(String input) {
         return (tokens.isEmpty() && input.charAt(0) == IRC.Constants.SOURCEPREPEND) ||
                 (tokens.size() == 1 && contains(IRC.Type.TAG) && input.charAt(0) == IRC.Constants.SOURCEPREPEND);
-    };
-
-    private boolean isCommand(){
-        return !contains(IRC.Type.COMMAND) &&
-                (tokens.isEmpty() && !contains(IRC.Type.TAG) && !contains(IRC.Type.SOURCE)) ||
-                (tokens.size() == 1 && contains(IRC.Type.TAG) || contains(IRC.Type.SOURCE)) ||
-                (tokens.size() == 2 && contains(IRC.Type.TAG) && contains(IRC.Type.SOURCE));
     }
 
-    private boolean isParameter(){
-        System.out.println(tokens.size());
+    ;
+
+    private boolean endOfLine() {
+        return input.substring(current).equals("\\r\\n");
+    }
+
+    private boolean isCommand(String token) {
+        return !contains(IRC.Type.COMMAND) && (
+                (tokens.isEmpty() && !isTag(token) && !isSource(token)) ||
+                        (tokens.size() == 1 && contains(IRC.Type.TAG) || contains(IRC.Type.SOURCE)) ||
+                        (tokens.size() == 2 && contains(IRC.Type.TAG) && contains(IRC.Type.SOURCE)));
+    }
+
+    private boolean isParameter() {
         return tokens.size() >= 1 && contains(IRC.Type.COMMAND);
     }
 
-    private boolean contains(IRC.Type type){
-        for(Token token : tokens){
-            if(token.getType() == type){
+    private boolean contains(IRC.Type type) {
+        for (Token token : tokens) {
+            if (token.getType() == type) {
                 return true;
             }
-        } return false;
+        }
+        return false;
     }
 
-    private boolean finished(){
+    private boolean finished() {
         return current >= input.length();
-    };
+    }
+
+    ;
 
     // TODO: Most of these functions should be private, so need a rewrite.
-    public boolean containsCRLF(){
+    public boolean containsCRLF() {
         return input.endsWith("\\r\\n");
     }
 
-    public boolean inputTooLong(){
+    public boolean inputTooLong() {
         // Maximum message length for IRC is 512 bytes
-        return input.length() > IRC.Constants.MAXLENGTH ;
-    }
-
-    public String stripCRLF(){
-        if (containsCRLF()){
-            return input.substring(0, input.length() - 4);
-        } throw new InputMismatchException("No CRLF");
+        return input.length() > IRC.Constants.MAXLENGTH;
     }
 }
