8146c2aa3ea190b45f221f14864b3ed7a9a18c78
Charlie <charlievanbeck@gmail.com>
2026-02-26 21:20:00 +0000

    Increase test coverage to further test tokenisation

--- src/test/java/com/github/cvanb002/IrcTest/ParserTest.java
+++ src/test/java/com/github/cvanb002/IrcTest/ParserTest.java
@@ -28,17 +28,6 @@ public class ParserTest {
         assertFalse(scanner.containsCRLF());
     }
 
-    @Test
-    public void scannerStripsCRLF(){
-        String containsCRLF = ":alice!alice@host PRIVMSG #chatroom :Hello!\\r\\n";
-        String notContainsCRLF = ":alice!alice@host PRIVMSG #chatroom :Hello!";
-
-        Scanner scanner = new Scanner(containsCRLF);
-        assertEquals(notContainsCRLF, scanner.stripCRLF());
-
-        scanner = new Scanner(notContainsCRLF);
-        assertThrows(InputMismatchException.class, scanner::stripCRLF);
-    }
 
     @Test
     public void ignoresMessagesGreaterThan512Chars(){
@@ -53,44 +42,138 @@ public class ParserTest {
 
     public void checkEqual(List<Token> first, List<Token> second){
         for(int i = 0; i < first.size(); i++){
+            System.out.println(first.get(i).getToken() + " : " + second.get(i).getToken());
+            System.out.println(first.get(i).getType() + " : " + second.get(i).getType());
             assertEquals(first.get(i).getType(), second.get(i).getType());
             assertEquals(first.get(i).getToken(), second.get(i).getToken());
         }
+            System.out.println(" ");
     }
 
 
     @Test
-    public void tokeniserSplitsCorrectly(){
-        List<Token> tokenised = new ArrayList<>();
+    public void tokeniseCommand(){
+        List<Token> tokens;
+        Scanner scanner = new Scanner("CAP\\r\\n");
+        tokens = scanner.scanTokens();
 
-        String input = ":alice!alice@host PRIVMSG #chatroom :Hello! How are you";
-        List<Token> correctOutput = new ArrayList<>()  ;
-        correctOutput.add(new Token(":alice!alice@host", IRC.Type.SOURCE));
-        correctOutput.add(new Token("PRIVMSG", IRC.Type.COMMAND));
-        correctOutput.add(new Token("#chatroom", IRC.Type.PARAMETER));
-        correctOutput.add(new Token(":Hello! How are you", IRC.Type.PARAMETER));
-        Scanner scanner = new Scanner(input);
-        tokenised = scanner.scanTokens();
-        checkEqual(correctOutput, tokenised);
+        List<Token> expected = new ArrayList<>();
+        expected.add(new Token("CAP", IRC.Type.COMMAND));
 
-        input = "CAP REQ :sasl message-tags foo";
-        correctOutput = new ArrayList<>()  ;
-        correctOutput.add(new Token("CAP", IRC.Type.COMMAND));
-        correctOutput.add(new Token("REQ", IRC.Type.PARAMETER));
-        correctOutput.add(new Token(":sasl message-tags foo", IRC.Type.PARAMETER));
-        scanner = new Scanner(input);
-        tokenised = scanner.scanTokens();
-        checkEqual(correctOutput, tokenised);
+        checkEqual(expected, tokens);
+    }
 
-        input = "@id=234AB :dan!d@localhost PRIVMSG #chan :Hey what's up!";
-        correctOutput = new ArrayList<>()  ;
-        correctOutput.add(new Token("@id=234AB", IRC.Type.TAG));
-        correctOutput.add(new Token(":dan!d@localhost", IRC.Type.SOURCE));
-        correctOutput.add(new Token("PRIVMSG", IRC.Type.COMMAND));
-        correctOutput.add(new Token("#chan", IRC.Type.PARAMETER));
-        correctOutput.add(new Token(":Hey what's up!", IRC.Type.PARAMETER));
-        scanner = new Scanner(input);
-        tokenised = scanner.scanTokens();
-        checkEqual(correctOutput, tokenised);
+    @Test
+    public void tokeniseParameter(){
+        List<Token> tokens;
+        Scanner scanner = new Scanner("CAP REQ\\r\\n");
+        tokens = scanner.scanTokens();
+
+        List<Token> expected = new ArrayList<>();
+        expected.add(new Token("CAP", IRC.Type.COMMAND));
+        expected.add(new Token("REQ", IRC.Type.PARAMETER));
+
+        checkEqual(expected, tokens);
+    }
+
+    @Test
+    public void tokeniseMultipleParameter(){
+        List<Token> tokens;
+        Scanner scanner = new Scanner("CAP REQ ANOTHER PARAM\\r\\n");
+        tokens = scanner.scanTokens();
+
+        List<Token> expected = new ArrayList<>();
+        expected.add(new Token("CAP", IRC.Type.COMMAND));
+        expected.add(new Token("REQ", IRC.Type.PARAMETER));
+        expected.add(new Token("ANOTHER", IRC.Type.PARAMETER));
+        expected.add(new Token("PARAM", IRC.Type.PARAMETER));
+
+        checkEqual(expected, tokens);
+    }
+
+    @Test
+    public void tokeniseExtParameter(){
+        List<Token> tokens;
+        Scanner scanner = new Scanner("CAP :sasl message-tags foo\\r\\n");
+        tokens = scanner.scanTokens();
+
+        List<Token> expected = new ArrayList<>();
+        expected.add(new Token("CAP", IRC.Type.COMMAND));
+        expected.add(new Token(":sasl message-tags foo", IRC.Type.PARAMETER));
+
+        checkEqual(expected, tokens);
+    }
+
+    @Test
+    public void tokeniseParamAndExtParameter(){
+        List<Token> tokens;
+        Scanner scanner = new Scanner("CAP REQ ANOTHER :sasl message-tags foo\\r\\n");
+        tokens = scanner.scanTokens();
+
+        List<Token> expected = new ArrayList<>();
+        expected.add(new Token("CAP", IRC.Type.COMMAND));
+        expected.add(new Token("REQ", IRC.Type.PARAMETER));
+        expected.add(new Token("ANOTHER", IRC.Type.PARAMETER));
+        expected.add(new Token(":sasl message-tags foo", IRC.Type.PARAMETER));
+
+        checkEqual(expected, tokens);
+    }
+
+    @Test
+    public void tokeniseSource(){
+        List<Token> tokens;
+        Scanner scanner = new Scanner(":alice!alice@host PRIVMSG #chatroom\\r\\n");
+        tokens = scanner.scanTokens();
+
+        List<Token> expected = new ArrayList<>();
+        expected.add(new Token(":alice!alice@host", IRC.Type.SOURCE));
+        expected.add(new Token("PRIVMSG", IRC.Type.COMMAND));
+        expected.add(new Token("#chatroom", IRC.Type.PARAMETER));
+
+        checkEqual(expected, tokens);
+    }
+
+    @Test
+    public void tokeniseSourceAndExtParameter(){
+        List<Token> tokens;
+        Scanner scanner = new Scanner(":alice!alice@host PRIVMSG #chatroom :Hello! How are you\\r\\n");
+        tokens = scanner.scanTokens();
+
+        List<Token> expected = new ArrayList<>();
+        expected.add(new Token(":alice!alice@host", IRC.Type.SOURCE));
+        expected.add(new Token("PRIVMSG", IRC.Type.COMMAND));
+        expected.add(new Token("#chatroom", IRC.Type.PARAMETER));
+        expected.add(new Token(":Hello! How are you", IRC.Type.PARAMETER));
+
+        checkEqual(expected, tokens);
+    }
+
+    @Test
+    public void tokeniseTag(){
+        List<Token> tokens;
+        Scanner scanner = new Scanner("@id=234AB PRIVMSG\\r\\n");
+        tokens = scanner.scanTokens();
+
+        List<Token> expected = new ArrayList<>();
+        expected.add(new Token("@id=234AB", IRC.Type.TAG));
+        expected.add(new Token("PRIVMSG", IRC.Type.COMMAND));
+
+        checkEqual(expected, tokens);
+    }
+
+    @Test
+    public void tokeniseTagSourceParams(){
+        List<Token> tokens;
+        Scanner scanner = new Scanner("@id=234AB :dan!d@localhost PRIVMSG #chan :Hey what's up!\\r\\n");
+        tokens = scanner.scanTokens();
+
+        List<Token> expected = new ArrayList<>();
+        expected.add(new Token("@id=234AB", IRC.Type.TAG));
+        expected.add(new Token(":dan!d@localhost", IRC.Type.SOURCE));
+        expected.add(new Token("PRIVMSG", IRC.Type.COMMAND));
+        expected.add(new Token("#chan", IRC.Type.PARAMETER));
+        expected.add(new Token(":Hey what's up!", IRC.Type.PARAMETER));
+
+        checkEqual(expected, tokens);
     }
 }
