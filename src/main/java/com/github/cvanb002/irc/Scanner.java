89beea6a52c35dd94768b5ed082bc852df28336d
Charlie <charlievanbeck@gmail.com>
2026-02-26 13:04:54 +0000

    Add CRLF stripping

--- src/main/java/com/github/cvanb002/irc/Scanner.java
+++ src/main/java/com/github/cvanb002/irc/Scanner.java
@@ -1,4 +1,26 @@
 package com.github.cvanb002.irc;
 
-public class Scanner {
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class Scanner{
+    String input = "";
+    List<String> tokens = new ArrayList<>();
+
+    public Scanner(String input){
+        this.input = input;
+    }
+
+    public boolean checkCRLF(){
+        return input.endsWith("\\r\\n");
+    }
+
+    public String stripCRLF(){
+        if (checkCRLF()){
+            return input.substring(0, input.length() - 4);
+        } throw new InputMismatchException("No CRLF");
+    }
+
+
 }
