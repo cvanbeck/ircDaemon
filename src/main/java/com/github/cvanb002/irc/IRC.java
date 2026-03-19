e78a65f0d8f676c69106c58f8cbf3c6481ba2855
Charlie <charlievanbeck@gmail.com>
2026-03-19 13:40:47 +0000

    Add all numerics specified in planning brief

--- src/main/java/com/github/cvanb002/irc/IRC.java
+++ src/main/java/com/github/cvanb002/irc/IRC.java
@@ -11,10 +11,23 @@ public class IRC {
     }
 
     public enum Numeric {
-        SUCCESS(999), // Not an actual IRC command, use to confirm to server success,
+        SUCCESS(-1), // Not an actual IRC command, use to confirm to server success,
         ERR_INPUT_TOO_LONG(417),
         ERR_UNKNOWNERROR(400),
-        ERR_NEEDMOREPARAMS(412);
+        ERR_NEEDMOREPARAMS(412),
+        ERR_ALREADYREGISTERED(462),
+        ERR_PASSWDMISMATCH(464),
+        ERR_NOOPERHOST(491),
+        RPL_YOUREOPER(381),
+        ERR_NOSUCHCHANNEL(403),
+        ERR_CHANOPRIVISNEEDED(482),
+        ERR_USERNOTINCHANNEL(441),
+        ERR_NOTONCHANNEL(442),
+        ERR_NOSUCHNICK(401),
+        ERR_NOSUCHSERVER(402),
+        ERR_CANNOTSENDTOCHAN(404),
+        ERR_NORECPIENT(411),
+        ERR_NOTEXTTOSEND(412);
 
         private final int code;
 
