809472b5781a238fd361a96507cff412c8dfea2a
Charlie <charlievanbeck@gmail.com>
2026-04-02 14:56:54 +0100

    Add message class to be used instead of Tokens for message parsing

---
+++ src/main/java/com/github/cvanb002/model/Message.java
@@ -0,0 +1,38 @@
+package com.github.cvanb002.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Message {
+    private String source;
+    private String command;
+    private List<String> parameters = new ArrayList<>();
+
+    public Message(String command){
+        this.command = command;
+    }
+
+    public Message(String command, List<String> parameters){
+        this.command = command;
+        this.parameters.addAll(parameters);
+    };
+
+
+    public Message(String source, String command, List<String> parameters){
+        this.source = source;
+        this.command = command;
+        this.parameters.addAll(parameters);
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public List<String> getParameters() {
+        return parameters;
+    }
+}
