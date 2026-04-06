3d887c4f9d71f5157d2515dd039ce3831eb851c7
Charlie <charlievanbeck@gmail.com>
2026-04-06 16:35:26 +0100

    Restructure ircDaemon initalisation
    Main file now intialises state, the parser/scanner, and the command handler before parsing this on when intialising Server

--- src/main/java/com/github/cvanb002/ircDaemon.java
+++ src/main/java/com/github/cvanb002/ircDaemon.java
@@ -1,6 +1,8 @@
 package com.github.cvanb002;
 
 
+import com.github.cvanb002.irc.Scanner;
+import com.github.cvanb002.mediator.CommandHandler;
 import com.github.cvanb002.server.MessageRouter;
 import com.github.cvanb002.server.Server;
 import com.github.cvanb002.server.State;
@@ -10,10 +12,12 @@ import java.io.IOException;
 public class ircDaemon {
     public static void main(String[] args){
         int port = 6665;
-        State state = new State();
-        MessageRouter router = new MessageRouter(state);
 
-        Server server = new Server(port, state, router);
+        State state = new State();
+        Scanner scanner = new Scanner();
+        CommandHandler commandHandler = new CommandHandler(state);
+
+        Server server = new Server(port, state, scanner, commandHandler);
         try{
             server.run();
         } catch (IOException e) {
--- src/main/java/com/github/cvanb002/server/MessageRouter.java
+++ src/main/java/com/github/cvanb002/server/MessageRouter.java
@@ -13,41 +13,5 @@ public class MessageRouter {
     public MessageRouter(State state){
         this.state = state;
     }
-
-    public void messageRecieved(String message, Client sender){
-        Scanner scanner = new Scanner();
-        Message scannedMessage = scanner.parse(message);
-        System.out.println(scannedMessage.toString());
-    }
-
-
-    public void sendMessage(String source, String message){
-        try{
-            Client client = resolveSource(source);
-            client.respond(message);
-        } catch (Exception e) {
-            // Placeholder exception, needs to throw an error if client not found
-            throw new RuntimeException(e);
-        }
-    }
-
-    public Client resolveSource(String source){
-        List<Client> clients = state.getClients();
-        for(Client client : clients) {
-            if(Objects.equals(client.getUser(), source)){
-                return client;
-            }
-        }
-        return null;
-    }
-
-    // Deprecated, can be used for testing purposes and sending message to all clients
-    public void sendMessage(String message, Client sender){
-        List<Client> clients = state.getClients();
-        for(Client client : clients) {
-            if (sender != client) {
-                client.respond(message);
-            }
-        }
-    }
+    
 }
