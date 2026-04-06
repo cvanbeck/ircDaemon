1e49c5764effa25344b649bc501cb04bf59adbc1
Charlie <charlievanbeck@gmail.com>
2026-04-06 16:07:57 +0100

    Fix incorrect input type for handle

--- src/main/java/com/github/cvanb002/mediator/CommandHandler.java
+++ src/main/java/com/github/cvanb002/mediator/CommandHandler.java
@@ -2,15 +2,16 @@ package com.github.cvanb002.mediator;
 
 
 import com.github.cvanb002.irc.IRC;
+import com.github.cvanb002.model.Message;
 import com.github.cvanb002.server.Client;
 import com.github.cvanb002.server.MessageRouter;
 
 import java.util.List;
 
 public class CommandHandler {
-    public void handle(String message, Client client){
+    public void handle(Message message, Client client){
         // temp implementation
-        client.send("DEBUG: Message recieved" + message);
+        client.send("DEBUG: Message recieved" + message.toString());
     };
 
     private IRC.Numeric ping(String source, String token){
