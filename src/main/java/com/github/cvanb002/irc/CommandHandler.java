04f87b20089455371dfaf53b490c51b9b2df2a2b
Charlie <charlievanbeck@gmail.com>
2026-04-08 12:06:52 +0100

    Add register method that loops through a list of commands and adds them to CommandHandler

--- src/main/java/com/github/cvanb002/irc/CommandHandler.java
+++ src/main/java/com/github/cvanb002/irc/CommandHandler.java
@@ -1,12 +1,18 @@
 package com.github.cvanb002.irc;
 
 
+import com.github.cvanb002.model.Command;
 import com.github.cvanb002.model.Message;
 import com.github.cvanb002.model.Client;
 import com.github.cvanb002.model.State;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class CommandHandler {
     private State state;
+    private HashMap<String, Command> commands = new HashMap<>();
 
     public CommandHandler(State state){
         this.state = state;
@@ -14,7 +20,15 @@ public class CommandHandler {
 
     public void handle(Message message, Client client){
             debugSending(message, client);
+    }
+
+    public void register(IRC.Commands commands){
+        for(IRC.Commands command: commands.values()){
+            Command commandClass = command.create(state);
+            this.commands.put(command.name(), commandClass);
         }
+    }
+
 
     private void debugSending(Message message, Client client){
         client.send("DEBUG: Message recieved" + message.toString());
