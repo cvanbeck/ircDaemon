3b66cf1bd010797ec3be2c05357a98dca4f5943e
Charlie <charlievanbeck@gmail.com>
2026-04-08 13:10:36 +0100

    Update handle method to lookup if command sent in message exists, and then call the command

--- src/main/java/com/github/cvanb002/irc/CommandHandler.java
+++ src/main/java/com/github/cvanb002/irc/CommandHandler.java
@@ -6,24 +6,29 @@ import com.github.cvanb002.model.Message;
 import com.github.cvanb002.model.Client;
 import com.github.cvanb002.model.State;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class CommandHandler {
-    private State state;
-    private HashMap<String, Command> commands = new HashMap<>();
+    private final State state;
+    private final HashMap<String, Command> commands = new HashMap<>();
 
     public CommandHandler(State state){
         this.state = state;
     }
 
     public void handle(Message message, Client client){
-            debugSending(message, client);
+            Command command = commands.get(message.getCommand().toUpperCase());
+            if(command != null){
+                command.call(message, client);
+            }
+            else {
+                debugSending(message, client);
+            }
     }
 
-    public void register(IRC.Commands commands){
-        for(IRC.Commands command: commands.values()){
+
+    public void register(IRC.Commands[] commands){
+        for(IRC.Commands command: commands){
             Command commandClass = command.create(state);
             this.commands.put(command.name(), commandClass);
         }
@@ -31,14 +36,11 @@ public class CommandHandler {
 
 
     private void debugSending(Message message, Client client){
-        client.send("DEBUG: Message recieved" + message.toString());
-        client.send(message.getCommand());
-        client.send(message.getSource());
         client.send("DEBUG Message recieved: " + message.toString());
-        client.send(message.getCommand());
-        client.send(message.getSource());
+        client.send("COMMAND: " + message.getCommand());
+        client.send("SOURCE: " + message.getSource());
         for(String param : message.getParameters()){
-            client.send(param);
+            client.send("PARAM: " + param);
         }
     }
 }
