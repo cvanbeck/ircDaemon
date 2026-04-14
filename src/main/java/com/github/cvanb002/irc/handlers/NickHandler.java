4d0bfe0de8c7bcf5195f244d4d9d3450e357f3e9
Charlie <charlievanbeck@gmail.com>
2026-04-14 11:35:17 +0100

    Add implementation for NICK command

--- src/main/java/com/github/cvanb002/irc/handlers/NickHandler.java
+++ src/main/java/com/github/cvanb002/irc/handlers/NickHandler.java
@@ -1,4 +1,28 @@
 package com.github.cvanb002.irc.handlers;
 
-public class NickHandler {
+import com.github.cvanb002.model.Client;
+import com.github.cvanb002.model.Command;
+import com.github.cvanb002.model.Message;
+import com.github.cvanb002.model.State;
+
+public class NickHandler extends Command {
+    State state;
+
+    public NickHandler(State state){
+        this.state = state;
+    }
+
+    @Override
+    public void call(Message message, Client client){
+        String nick = message.getParameters().get(0);
+        if(state.nickExists(nick)){
+            client.send("ERR_NICKNAMEINUSE");
+            return;
+        }
+
+        client.setNick(nick);
+        client.setHasNick(true);
+
+        client.attemptRegistration();
+        }
 }
