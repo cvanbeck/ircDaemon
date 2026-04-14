202e7994f1f9616daeb96a3ee783bd3a81ae8c35
Charlie <charlievanbeck@gmail.com>
2026-04-14 14:58:02 +0100

    Add basic PRIVMSG command, capable of directly messaging a user on the network

--- src/main/java/com/github/cvanb002/irc/handlers/PrivMsgHandler.java
+++ src/main/java/com/github/cvanb002/irc/handlers/PrivMsgHandler.java
@@ -14,6 +14,28 @@ public class PrivMsgHandler extends Handler {
 
     @Override
     public void call(Message message, Client client) {
-        return;
+        String nick = message.getParameters(0);
+
+        Client destination;
+
+        if(state.nickExists(nick)){
+            destination = state.findClient(nick);
+            Message response = new Message();
+            response.addSource(":" + client.getNick());
+            response.addCommand("PRIVMSG");
+            StringBuilder text = new StringBuilder(nick + " :");
+            String param;
+
+            for(int i = 1; i < message.getParameters().size(); i++){
+                param = message.getParameters(i);
+                text.append(" ");
+                text.append(param);
+            }
+
+            response.addParameter(String.valueOf(text));
+            destination.send(response.toString());
+        }
+
     }
+
 }
