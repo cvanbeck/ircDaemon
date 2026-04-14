5bdb9d6b85fd698cc260ae4cb4feabfc64d9e471
Charlie <charlievanbeck@gmail.com>
2026-04-14 12:45:26 +0100

    Inital commit for EXIT command

---
+++ src/main/java/com/github/cvanb002/irc/handlers/ExitHandler.java
@@ -0,0 +1,19 @@
+package com.github.cvanb002.irc.handlers;
+
+import com.github.cvanb002.model.Client;
+import com.github.cvanb002.model.Handler;
+import com.github.cvanb002.model.Message;
+import com.github.cvanb002.model.State;
+
+public class ExitHandler extends Handler {
+    State state;
+
+    public ExitHandler(State state){
+        this.state = state;
+    }
+
+    @Override
+    public void call(Message message, Client client){
+        
+    }
+}
